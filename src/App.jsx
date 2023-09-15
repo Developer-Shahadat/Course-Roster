88f8367054afefef1aa2b243cec522d2aa8f08b3
Developer-Shahadat <shahadatbci95@gmail.com>
2023-09-15 12:03:07 +0600

    total credit and remaining credit and total price and toast show display cart

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,33 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
   const [cartAdd,setCardAdd] = useState([]);
+  const[totalCredit,setTotalCredit] = useState([0]);
+  const [remaining,setRemaining] = useState([0]);
+  const [totalPrice,setTotalPrice] = useState([0]);
       const handleSelect = (cartItem) => {
       const newCardItem = [...cartAdd,cartItem];
       const isExist = cartAdd.find((card)=> card.id === cartItem.id )
+      let credit = cartItem.credit;
+      let price = cartItem.price;
         if(isExist){
           toast.error('Course already added to the cart');
         }
         else{
-          setCardAdd(newCardItem);
-        }
+          cartAdd.forEach(creditAdd => {
+            credit = credit + creditAdd.credit;
+            price = price + creditAdd.price
+          });
+          const totalRemaining = 20 - credit
+          if(credit > 20 ){
+            toast.error("Can't be more than 20hr added" );
+          }
+          else{
+            setTotalCredit(credit);
+            setRemaining(totalRemaining);
+            setCardAdd(newCardItem);
+            setTotalPrice(price);
+          }
+       }
       }
 
   return (
@@ -24,7 +42,7 @@ function App() {
       <Header></Header>
         <div className="flex container mx-auto gap-10 ">
           <Card handleSelect={handleSelect}></Card>
-          <Carts cartAdd={cartAdd}></Carts>
+          <Carts cartAdd={cartAdd} totalCredit={totalCredit} remaining={remaining} totalPrice ={totalPrice}></Carts>
         </div>
       </div>
      
--- src/components/Cart/Carts.jsx
+++ src/components/Cart/Carts.jsx
@@ -1,13 +1,13 @@
 import Cart from './Cart';
 import PropTypes from 'prop-types';
-const Carts = ({cartAdd}) => {
+const Carts = ({cartAdd,totalCredit,remaining,totalPrice}) => {
     return (
         <div className="">
             <div className="mt-8 card  bg-base-100 shadow-lg pt-4 pb-4">
                 <div className="mx-4">
-                    <h3>Credit Hour Remaining</h3>
-                    <hr className="mt-2" />
-                    <h2 className="font-semibold text-lg">Course Name</h2>
+                    <h3 className='text-[#2F80ED] font-bold'>Credit Hour Remaining  {remaining} hr</h3>
+                    <hr className="mt-3" />
+                    <h2 className="font-semibold text-lg mt-2">Course Name</h2>
                     {
                         cartAdd.map((cartAdd,index)=> <ol className='text-gray-400 mt-1' key={index}><Cart key ={index} cartId={cartAdd}>
                         </Cart>{index+1}. {cartAdd.course_name}</ol>
@@ -19,14 +19,19 @@ const Carts = ({cartAdd}) => {
                             {index + 1}. <Cart course={course}>{course.course_name}</Cart>
                         </ol>
                     ))} */}
-                    <hr className="mt-2"/>
-                    <h3>Total Credit Hour :</h3>
+                    <hr className="mt-4"/>
+                    <h3 className='font-medium mt-2'>Total Credit Hour : {totalCredit}</h3>
+                    <hr className='mt-2' />
+                    <h3 className='font-semibold mt-2'>Total Price : {totalPrice} USD</h3>
                 </div>
             </div>
         </div>
     );
 };
 Carts.propTypes = {
-    cartAdd: PropTypes.array.isRequired
+    cartAdd: PropTypes.array.isRequired,
+    totalCredit: PropTypes.object.isRequired,
+    remaining : PropTypes.object.isRequired,
+    totalPrice : PropTypes.object.isRequired
 }
 export default Carts;
